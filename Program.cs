8932abaca5b29dccd93bbcbd8b2758ceec89b794
Katia Sousa <katiafesousa@outlook.com>
2022-11-11 21:42:30 -0300

    POO - Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "86598", modelo: "Model1", imei: "111111", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "67890", modelo: "Model2", imei: "99999", memoria: 240);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("youtube");
